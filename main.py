c7b58c114524ec2561995c738601b8018a2d89d8
y112t521@ku.edu <Yuter132546!>
2019-08-12 20:56:01 -0600

    Add working stock manipulation

--- main.py
+++ main.py
@@ -2,20 +2,43 @@ import datetime as dt
 import matplotlib.pyplot as plt
 from matplotlib import style
 import pandas as pd
+from mpl_finance import candlestick_ohlc
 import pandas_datareader.data as web
+import matplotlib.dates as mdates
 
 style.use('ggplot')
 
-start = dt.datetime(2009, 1, 1)
-end = dt.datetime.now()
+# write the history of a stock to a file
+# start = dt.datetime(2009, 1, 1)
+# end = dt.datetime.now()
+# df = web.DataReader("AAPL", 'yahoo', start, end)
+# df.to_csv('AAPL.csv')
 
-df = web.DataReader("AAPL", 'yahoo', start, end)
-df.to_csv('AAPL.csv')
+
 # df = pd.read_csv('DOX.csv')
-#df = pd.read_csv('tsla.csv', parse_dates=True, index_col=0)
-print(df.head())
-##df['Adj CLose'].plot()
+df = pd.read_csv('AAPL.csv', parse_dates=True, index_col=0)
+# print(df.head())
+df_ohlc = df['Adj Close'].resample('10D').ohlc()
+df_volume = df['Volume'].resample('10D').sum()
+
+df_ohlc.reset_index(inplace=True)
+df_ohlc['Date'] = df_ohlc['Date'].map(mdates.date2num)
+
+print(df_ohlc.head())
+#print(df_volume)
+
+ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
+ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex=ax1)
+ax1.xaxis_date()
+
+
+candlestick_ohlc(ax1, df_ohlc.values, width=2, colorup ='g')
+ax2.fill_between(df_volume.index.map(mdates.date2num), df_volume.values, 0)
+
+
 plt.show()
+##df['Adj CLose'].plot()
+#plt.show()
 # df['100ma'] = df['Adj Close'].rolling(window =100, min_periods=0).mean()
 # df.dropna(inplace=True)
 # print(df.head())
