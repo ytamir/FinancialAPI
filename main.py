69890d8a9ded4a8c66afecceea1be8b58ea17c80
Yarden Tamir <ytamir18@gmail.com>
2019-08-14 19:39:26 -0500

    Add React Page to Project

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import datetime as dt
-import matplotlib.pyplot as plt
-from matplotlib import style
-import pandas as pd
-from mpl_finance import candlestick_ohlc
-import pandas_datareader.data as web
 import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib import style
+from mpl_finance import candlestick_ohlc
 
 style.use('ggplot')
 
