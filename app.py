e8a4455de64d226931081222399cfe88113e17d8
Yarden Tamir <ytamir18@gmail.com>
2019-08-21 18:11:03 -0500

    change procfile

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-
+import os
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -6,6 +7,8 @@ import dash_html_components as html
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+server = app.server
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
