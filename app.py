40da435cbbdf7558c9e0dec08d0b3568ced6ce88
Yarden Tamir <ytamir18@gmail.com>
2019-08-24 15:18:23 -0500

    Add working dynamic dataframe to the webapp

--- app.py
+++ app.py
@@ -3,6 +3,31 @@ import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
+import dash_table
+# from GetStockRowData import StockRowData
+import plotly.graph_objs as go
+from stock import Stocks
+
+
+df = Stocks.getdata()
+# df = StockRowData.get_file("GOOGL", SheetType.METRICS, Frequency.QUARTERLY)
+# df['Short Date']= pd.to_datetime(df['DATE'])
+# df['Short Date'] = df['Short Date'].apply(lambda x:x.date().strftime('%m%d%y'))
+# df.to_csv("test.csv", index=False)
+
+df.to_csv('test.csv')
+fd = pd.read_csv("test.csv")
+dff = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
+print("df")
+print (df)
+print('dff')
+print(dff)
+print('fd')
+print(fd)
+#print('fd')
+#print(fd)
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -11,24 +36,20 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='AAPL Stock'),
 
     html.Div(children='''
         Dash: A web application framework for Python.
     '''),
 
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
+   dcc.Graph(
+    id='table',
+    figure = {
+        'data': [
+            go.Scatter(x=fd['Date'], y=fd['High'])
+        ]
         }
-    )
+)
 ])
 
 if __name__ == '__main__':
---
+++ stock.py
@@ -0,0 +1,27 @@
+import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pandas as pd
+import pandas_datareader.data as web
+
+class Stocks:
+    def getdata():
+        style.use('ggplot')
+
+        start = dt.datetime(2009, 1, 1)
+        end = dt.datetime.now()
+
+        df = web.DataReader("AAPL", 'yahoo', start, end)
+
+        #df.to_csv('AAPL.csv')
+        # df = pd.read_csv('DOX.csv')
+        #df = pd.read_csv('tsla.csv', parse_dates=True, index_col=0)
+        #print(df.head())
+        ##df['Adj CLose'].plot()
+        #plt.show()
+        return df
+        # df['100ma'] = df['Adj Close'].rolling(window =100, min_periods=0).mean()
+        # df.dropna(inplace=True)
+        # print(df.head()) 
+instance = Stocks()
+instance.getdata
