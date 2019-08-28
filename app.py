9826265917090db714cbf49b83f9df9859788dd1
Yarden Tamir <ytamir18@gmail.com>
2019-08-27 23:31:05 -0500

    Add Working Stock Picker

--- app.py
+++ app.py
@@ -8,26 +8,60 @@ import dash_table
 # from GetStockRowData import StockRowData
 import plotly.graph_objs as go
 from stock import Stocks
+from dash.dependencies import Input, Output, State
 
 
-df = Stocks.getdata()
+
+#df = Stocks.getdata("AAPL")
 # df = StockRowData.get_file("GOOGL", SheetType.METRICS, Frequency.QUARTERLY)
 # df['Short Date']= pd.to_datetime(df['DATE'])
 # df['Short Date'] = df['Short Date'].apply(lambda x:x.date().strftime('%m%d%y'))
 # df.to_csv("test.csv", index=False)
 
-df.to_csv('test.csv')
-fd = pd.read_csv("test.csv")
+
+
+
+#df.to_csv('aapl.csv')
+fd = pd.read_csv("aapl.csv")
 dff = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
-print("df")
-print (df)
-print('dff')
-print(dff)
-print('fd')
-print(fd)
+
+nasdaqdf = pd.read_csv("nasdaq.csv")
+nysedf = pd.read_csv("nyse.csv")
+symbols = nysedf.Symbol.values.tolist() + nasdaqdf.Symbol.values.tolist()
+
 #print('fd')
 #print(fd)
 
+
+
+WELL_STATUSES = dict(
+    AC = 'Active',
+    AR = 'Application Received to Drill/Plug/Convert',
+    CA = 'Cancelled',
+    DC = 'Drilling Completed',
+    DD = 'Drilled Deeper',
+    DG = 'Drilling in Progress',
+    EX = 'Expired Permit',
+    IN = 'Inactive',
+    NR = 'Not Reported on AWR',
+    PA = 'Plugged and Abandoned',
+    PI = 'Permit Issued',
+    PB = 'Plugged Back',
+    PM = 'Plugged Back Multilateral',
+    RE = 'Refunded Fee',
+    RW = 'Released - Water Well',
+    SI = 'Shut-In',
+    TA = 'Temporarily Abandoned',
+    TR = 'Transferred Permit',
+    UN = 'Unknown',
+    UL = 'Unknown Located',
+    UM = 'Unknown Not Found',
+    VP = 'Voided Permit',
+)
+
+well_status_options = [{'label': str(a),
+                        'value': str(a)}
+                       for a in symbols]
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -36,11 +70,14 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 app.layout = html.Div(children=[
-    html.H1(children='AAPL Stock'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
+    html.H1(children='AAPL', id='name'),
+    dcc.Dropdown(
+                            id='well_statuses',
+                            options=well_status_options,
+                            multi=True,
+                            className="dcc_control"
+                        ),
+    html.Button(id='submit-button', n_clicks=0, children='Submit'),
 
    dcc.Graph(
     id='table',
@@ -51,6 +88,56 @@ app.layout = html.Div(children=[
         }
 )
 ])
+
+
+
+
+@app.callback([Output('table', 'figure'),
+               Output('name','children')],              
+              [Input('submit-button', 'n_clicks')],
+              [State('well_statuses', 'value')])
+def update_output(n_clicks, input_symbol):
+    if input_symbol is not None:
+        newdf = Stocks.getdata(input_symbol)
+        print(newdf)
+        newdf.to_csv('test.csv')
+        fdd = pd.read_csv("test.csv")
+        #output_name = input_symbol.append(" Stock")
+        return {
+            'data': [
+                go.Scatter(x=fdd['Attributes'], y=fdd['High'])
+            ]
+        }, input_symbol
+    else:
+        return {
+                   'data': [
+                       go.Scatter(x=fd['Date'], y=fd['High'])
+                   ]
+               }, "AAPL"
+
+
+
+
+
+
+# @app.callback(
+#     Output('table', 'figure' ),
+#     [Input('well_statuses', 'value')]
+# )
+# def update_graph(input_symbol):
+#     if input_symbol in symbols:
+#         newdf = Stocks.getdata(input_symbol)
+#         newdf.to_csv('test.csv')
+#         fdd = pd.read_csv("test.csv")
+#         return {
+#             'data': [
+#                 go.Scatter(x=fd['Date'], y=fdd['High'])
+#             ]
+#         }
+
+
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- stock.py
+++ stock.py
@@ -4,24 +4,65 @@ from matplotlib import style
 import pandas as pd
 import pandas_datareader.data as web
 
+
 class Stocks:
     def getdata():
         style.use('ggplot')
 
         start = dt.datetime(2009, 1, 1)
         end = dt.datetime.now()
+        # try:
+        #     df = web.DataReader(symbol, 'yahoo', start, end)
+        #     print(df)
+        #     return df
+        # except:
+        #     return pd.read_csv('aapl.csv')
 
         df = web.DataReader("AAPL", 'yahoo', start, end)
-
-        #df.to_csv('AAPL.csv')
-        # df = pd.read_csv('DOX.csv')
-        #df = pd.read_csv('tsla.csv', parse_dates=True, index_col=0)
-        #print(df.head())
-        ##df['Adj CLose'].plot()
-        #plt.show()
         return df
+
+    def getdata(symbol):
+        style.use('ggplot')
+
+        start = dt.datetime(2009, 1, 1)
+        end = dt.datetime.now()
+        try:
+            df = web.DataReader(symbol, 'yahoo', start, end)
+            print(df)
+            return df
+        except:
+            return pd.read_csv('aapl.csv')
+
+        # df = web.DataReader(symbol, 'yahoo', start, end)
+        # return df
+
+        # df.to_csv('AAPL.csv')
+        # df = pd.read_csv('DOX.csv')
+        # df = pd.read_csv('tsla.csv', parse_dates=True, index_col=0)
+        # print(df.head())
+        ##df['Adj CLose'].plot()
+        # plt.show()
+
         # df['100ma'] = df['Adj Close'].rolling(window =100, min_periods=0).mean()
         # df.dropna(inplace=True)
-        # print(df.head()) 
-instance = Stocks()
-instance.getdata
+        # print(df.head())
+
+    def printdata(symbol):
+        style.use('ggplot')
+
+        start = dt.datetime(2009, 1, 1)
+        end = dt.datetime.now()
+        df = web.DataReader("DOX", 'yahoo', start, end)
+        print(df)
+        return df
+
+        # df.to_csv('AAPL.csv')
+        # df = pd.read_csv('DOX.csv')
+        # df = pd.read_csv('tsla.csv', parse_dates=True, index_col=0)
+        # print(df.head())
+        ##df['Adj CLose'].plot()
+        # plt.show()
+
+        # df['100ma'] = df['Adj Close'].rolling(window =100, min_periods=0).mean()
+        # df.dropna(inplace=True)
+        # print(df.head())
