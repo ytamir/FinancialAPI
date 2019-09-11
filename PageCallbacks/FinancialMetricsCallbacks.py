bb3f9115371c9b9154a8f80ffcd2a22c05680847
Yarden Tamir <ytamir18@gmail.com>
2019-09-11 18:57:13 -0500

    Add color by company to the metrics page, remove DATE from config

--- PageCallbacks/FinancialMetricsCallbacks.py
+++ PageCallbacks/FinancialMetricsCallbacks.py
@@ -1,6 +1,8 @@
 import plotly.graph_objs as go
+import numpy as np
 from PyhonRequestFiles import FinancialMetricsScraper
 from dash.dependencies import Input, Output
+
 
 
 # TODO transform metrics dropdown to candlestick with selected values
@@ -13,6 +15,10 @@ def register_callbacks(app, metrics_dict):
         trace = []
         if input_symbols is not None:
             for symbol in input_symbols:
+                color = "rgb"+str(tuple(list(np.random.choice(range(0, 200), size=3)))) # colors will need to change
+                # depending on the background color if styles are ever changed
+                color_dict = {"color": color}
+
                 for c in metrics_sel:
                     stock_data = FinancialMetricsScraper.StockRowData.get_file(symbol,
                                                                                FinancialMetricsScraper.SheetType.
@@ -24,6 +30,7 @@ def register_callbacks(app, metrics_dict):
                     # print(stock_data.head())
                     # print("metrics_dict[str(c)]")
                     # print(metrics_dict[str(c)])
+
                     trace.append(go.Scatter(x=stock_data.iloc[:, 0], y=stock_data.iloc[:, c], name=symbol + ": " + metrics_dict[str(c)],
-                                            mode='lines'))
+                                            mode='lines+markers', line=color_dict))
         return {"data": trace}
