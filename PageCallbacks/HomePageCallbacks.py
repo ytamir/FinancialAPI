1c813fdba3b990d1819fe2dfe5e094b661462d1b
Yarden Tamir <ytamir18@gmail.com>
2019-09-18 21:40:26 -0500

    Add aplha_advantage api

--- PageCallbacks/HomePageCallbacks.py
+++ PageCallbacks/HomePageCallbacks.py
@@ -13,19 +13,19 @@ def register_callbacks(app):
         trace = []
         if input_symbols is not None:
             for symbol in input_symbols:
-                stock_data = Stocks.getdata(symbol)
+                stock_data = Stocks.getdatadaily(symbol)
                 stock_data.to_csv('CSVFiles/test.csv')
                 stock_data_out = pd.read_csv("CSVFiles/test.csv")
                 if candle_val == 'C':
                     trace.append(
-                        go.Candlestick(x=stock_data_out['Date'], open=stock_data_out['Open'],
+                        go.Candlestick(x=stock_data_out['date'], open=stock_data_out['Open'],
                                        high=stock_data_out['High'],
                                        low=stock_data_out['Low'], close=stock_data_out['Close'], name=symbol,
                                        increasing={'line': {'color': '#00CC94'}},
                                        decreasing={'line': {'color': '#F50030'}}))
                 else:
                     trace.append(
-                        go.Scatter(x=stock_data_out['Date'], y=stock_data_out['High'], name=symbol, mode='lines'))
+                        go.Scatter(x=stock_data_out['date'], y=stock_data_out['High'], name=symbol, mode='lines'))
             if candle_val == 'C':
                 return {"data": trace, 'layout': go.Layout(title=f"Stock Values",
                                                            xaxis={'rangeslider': {'visible': False},
--- PyhonRequestFiles/stock.py
+++ PyhonRequestFiles/stock.py
@@ -3,6 +3,18 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 import pandas as pd
 import pandas_datareader.data as web
+from alpha_vantage.timeseries import TimeSeries
+from alpha_vantage.techindicators import TechIndicators
+import csv
+
+
+def getapikey():
+    with open('CSVFiles/apikey.csv') as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=',')
+        for row in readCSV:
+            print('row')
+            print(row)
+            return row[0]
 
 
 class Stocks:
@@ -21,9 +33,30 @@ class Stocks:
         end = dt.datetime.now()
         try:
             df = web.DataReader(symbol, 'yahoo', start, end)
-            return df
+            print(df.head())
+            stock_data = df
+            stock_data.to_csv('test.csv')
+            stock_data_out = pd.read_csv("test.csv")
+            print(stock_data_out['Date'].head())
+            # return df
+
         except:
             return pd.read_csv('CSVFiles/aapl.csv')
+
+    def getdatadaily(symbol):
+        key = getapikey()
+        ts = TimeSeries(key, output_format='pandas', indexing_type='date')
+        # data, meta_data = ts.get_intraday(symbol='MSFT',interval='1min', outputsize='full')
+        data, meta_data = ts.get_daily(symbol=symbol, outputsize='full')
+        data.rename(columns={'date': 'Date',
+                             '1. open': 'Open',
+                             '2. high': 'High',
+                             '3. low': 'Low',
+                             '4. close': 'Close',
+                             '5. volume': 'Volume'},
+                    inplace=True)
+        return data
+        #  df = df.T.reset_index(drop=True).T
 
     def printdata(symbol):
         style.use('ggplot')
@@ -33,3 +66,5 @@ class Stocks:
         df = web.DataReader("DOX", 'yahoo', start, end)
         print(df)
         return df
+
+
